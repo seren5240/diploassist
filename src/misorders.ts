06ba3db1fbd3ac96922fa1526d608292b21bf7fd
seren <chungkiukwok@gmail.com>
2022-08-06 07:46:45 +0800

    Move support move misorder colouring into SupportMove class

--- src/misorders.ts
+++ src/misorders.ts
@@ -1,79 +1,3 @@
-class SupportMove {
-  private convoyingUnit: Coordinate;
-  private midpoint: Coordinate;
-  private quarterPoint: Coordinate;
-  private threeQuartersPoint: Coordinate;
-  private xInflectionIncrement: number;
-  private yInflectionIncrement: number;
-
-  constructor(
-    centerOfConvoyingUnit: Coordinate,
-    centerOfConvoyedUnit: Coordinate,
-    centerOfConvoyDestination: Coordinate
-  ) {
-    this.convoyingUnit = centerOfConvoyingUnit;
-    this.midpoint = {
-      x: (centerOfConvoyedUnit.x + centerOfConvoyDestination.x) / 2,
-      y: (centerOfConvoyedUnit.y + centerOfConvoyDestination.y) / 2,
-    };
-    this.quarterPoint = {
-      x: (3 * centerOfConvoyedUnit.x + centerOfConvoyDestination.x) / 4,
-      y: (3 * centerOfConvoyedUnit.y + centerOfConvoyDestination.y) / 4,
-    };
-    this.threeQuartersPoint = {
-      x: (centerOfConvoyedUnit.x + 3 * centerOfConvoyDestination.x) / 4,
-      y: (centerOfConvoyedUnit.y + 3 * centerOfConvoyDestination.y) / 4,
-    };
-    [this.xInflectionIncrement, this.yInflectionIncrement] =
-      this.setConvoyHelperInflectionIncrement(
-        centerOfConvoyedUnit,
-        centerOfConvoyDestination
-      );
-  }
-
-  private setConvoyHelperInflectionIncrement(
-    centerOfConvoyedUnit: Coordinate,
-    centerOfConvoyDestination: Coordinate
-  ): [number, number] {
-    if (centerOfConvoyDestination.x > centerOfConvoyedUnit.x) {
-      return [
-        0.05 * (centerOfConvoyDestination.y - centerOfConvoyedUnit.y),
-        0.05 * (centerOfConvoyedUnit.x - centerOfConvoyDestination.x),
-      ];
-    } else {
-      return [
-        0.05 * (centerOfConvoyedUnit.y - centerOfConvoyDestination.y),
-        0.05 * (centerOfConvoyDestination.x - centerOfConvoyedUnit.x),
-      ];
-    }
-  }
-
-  public getSupportMoveAssistPath(): string {
-    return [
-      "M",
-      this.convoyingUnit.x,
-      ",",
-      this.convoyingUnit.y,
-      "C",
-      this.midpoint.x,
-      ",",
-      this.midpoint.y,
-      " ",
-      this.quarterPoint.x + this.yInflectionIncrement,
-      ",",
-      this.quarterPoint.y + this.xInflectionIncrement,
-      " ",
-      this.threeQuartersPoint.x,
-      ",",
-      this.threeQuartersPoint.y,
-    ].join("");
-  }
-
-  public getSupportMoveAssistCircleCenter(): Coordinate {
-    return this.threeQuartersPoint;
-  }
-}
-
 function getCenterOfOriginFromOriginRegexArray(origin: string[]) {
   if (!origin[1]) {
     return territories[origin[0]].unit_center;
@@ -293,7 +217,7 @@ function colorSupportMoveMisorderRed(
   misorderPath: string,
   circleCenter: Coordinate
 ): void {
-  // Need to investigate why unminified js does not produce correct path
+  // Backstabbr applies additional transformation to support move paths after the extracted code
   d3.select("svg")
     .selectAll("path")
     .filter(function () {
@@ -442,17 +366,11 @@ function checkSupportMoveOrderForMisorder(
       centerOfSupportDestination
     );
 
-    const supportPath: string = support.getSupportMoveAssistPath();
-    const circleCenter: Coordinate = support.getSupportMoveAssistCircleCenter();
-    console.log(`support path is ${supportPath}`);
-    console.log(`circle center is ${(circleCenter.x, circleCenter.y)}`);
-    colorSupportMoveMisorderRed(supportPath, circleCenter);
-
-    // M279,283C342,282.5 349.6,302 332.5,266.25
-
-    /*
-"M279,283C342,282.5,354.75,296.85,332.5,266.25" stroke-dasharray="2,2" stroke-width="2" transform="matrix(1,0,0,1,0,0)" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></path>
-    */
+    // const supportPath: string = support.getSupportMoveAssistPath();
+    // const circleCenter: Coordinate = support.getSupportMoveAssistCircleCenter();
+    // console.log(`support path is ${supportPath}`);
+    // console.log(`circle center is ${(circleCenter.x, circleCenter.y)}`);
+    support.colorSupportMoveMisorderRed();
   }
 }
 
